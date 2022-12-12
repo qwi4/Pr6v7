e14eb23976b92e0baf652849d4372674bfa0f5ff
исп-31 <-31@LAB17-07>
2022-12-12 13:18:24 +0300

    Добавьте файлы проекта.

---
+++ Pr6v7/MainWindow.xaml.cs
@@ -0,0 +1,180 @@
+﻿using lib;
+using System;
+using System.Windows;
+
+namespace Pr6v7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mItAboutProgram_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Вариант 7. Создать класс Triad (тройка чисел). Создать необходимые методы и свойства. " +
+                "Определить метод сравнения триад: триада p1 больше триады р2, если(first.pl >" +
+                "first.р2) или(first.pl = first.р2) и(second.pl > second.р2) и(three.pl > three.р2)." +
+                "Создать перегруженные методы SetParams, для установки параметров объекта, в" +
+                "том числе увеличения всех чисел на 10.",
+             "Разработчик: 10.",
+             MessageBoxButton.OK,
+             MessageBoxImage.Information);
+        }
+
+        private void mItExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Добавление элементов в listbox с проверкой на корректность введенных данных с помощью конструкции try-catch
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Triad triada1 = new Triad(Convert.ToInt32(txtBoxFirstNumber.Text), Convert.ToInt32(txtBoxSecondNumber.Text), Convert.ToInt32(txtBoxThirdNumber.Text)); //создание целой триады из 3-х эл-ов
+                lBoxResult.Items.Add(triada1); //добавление целой триады из 3-х эл-ов
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message); //Вывод ошибки
+            }
+        }
+
+        /// <summary>
+        /// Очистка
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            lBoxResult.Items.Clear();
+            txtBoxFirstNumber.Clear();
+            txtBoxSecondNumber.Clear();
+            txtBoxThirdNumber.Clear();
+        }
+
+        /// <summary>
+        /// Сравнение 2-х выбранных триад
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnResult_Click(object sender, RoutedEventArgs e)
+        {
+            var selectTriad = lBoxResult.SelectedItems;
+
+            if (selectTriad.Count == 2)
+            {
+                Triad One = selectTriad[0] as Triad;
+                Triad Two = selectTriad[1] as Triad;
+
+                if (One.Compare(Two))
+                {
+                    MessageBox.Show($"Триада {One} больше триады {Two}",
+                    "Результат сравнения",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show($"Триада {One} меньше {Two}",
+                    "Результат сравнения",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выделите 2 триады!!!",
+                    "Ошибка",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+            }
+        }
+
+        /// <summary>
+        /// Перегрузки: увелечение эл-ов триад на 10
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCount_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedTriad = lBoxResult.SelectedItems;
+
+            if (selectedTriad.Count == 1)
+            {
+                Triad One = selectedTriad[0] as Triad;
+
+                One.SetParams();
+                lBoxResult.Items.Add(One);
+            }
+            else
+            {
+                MessageBox.Show("Выделите 1 триаду!!!",
+                    "Ошибка",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+            }
+        }
+
+        private void btnResult2_Click(object sender, RoutedEventArgs e)
+        {
+            var selectTriad = lBoxResult.SelectedItems;
+
+            if (selectTriad.Count == 2)
+            {
+                Triad One = selectTriad[0] as Triad;
+                Triad Two = selectTriad[1] as Triad;
+
+                if (One>(Two))
+                {
+                    MessageBox.Show($"Триада {One} больше триады {Two}",
+                    "Результат сравнения",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show($"Триада {One} меньше {Two}",
+                    "Результат сравнения",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выделите 2 триады!!!",
+                    "Ошибка",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+            }
+        }
+
+        private void btnCount2_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedTriad = lBoxResult.SelectedItems;
+
+            if (selectedTriad.Count == 1)
+            {
+                Triad One = selectedTriad[0] as Triad;
+                One++;
+                lBoxResult.Items.Add(One);
+            }
+            else
+            {
+                MessageBox.Show("Выделите 1 триаду!!!",
+                    "Ошибка",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ lib_7/Triad.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib
+{
+    /// <summary>
+    /// Класс для создания триад и работы с ними
+    /// </summary>
+    public class Triad
+    {
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        private int v1;
+        private int v2;
+        private int v3;
+
+
+        public Triad(int v1, int v2, int v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+        }
+
+        /// <summary>
+        /// автоматические св-ва
+        /// </summary>
+        public int One { get => v1; set => v1 = value; } //Первое число
+        public int Two { get => v2; set => v2 = value; } //Второе число
+        public int Three { get => v3; set => v3 = value; } //Третье число
+
+        /// <summary>
+        /// Сравнивание чисел в триадах
+        /// </summary>
+        /// <returns>true or false</returns>
+        public bool Compare(Triad triad)
+        {
+            return One > triad.One || One == triad.One && Two > triad.Two && Three > triad.Two;
+        }
+
+        /// <summary>
+        /// Правильный вывод триад
+        /// </summary>
+        /// <returns>триады One, Two. Three</returns>
+        public override string ToString()
+        {
+            return $"{One}, {Two}, {Three}";
+        }
+
+        /// <summary>
+        /// Увелечение значений на 10 (перегрузки)
+        /// </summary>
+        /// <param name="One"></param>
+        /// <param name="Two"></param>
+        /// <param name="Three"></param>
+        public void SetParams()
+        {
+            One += 10;
+            Two += 10;
+            Three += 10;
+        }
+
+        public void SetParams(int one)
+        {
+            One = one;
+        }
+
+        public void SetParams(int one, int two)
+        {
+            One = one;
+            Two = two;
+        }
+
+        /// <summary>
+        /// Перегрузка операторов 
+        /// </summary>
+        public static bool operator >(Triad t1, Triad t2)
+        {
+            if (t1.One > t2.One || t1.One == t2.One && t1.Two > t2.Two && t1.Three > t2.Two) return true;
+            else return false;
+            
+        }
+        public static bool operator <(Triad t1, Triad t2)
+        {
+            return false;
+        }
+
+       public static Triad operator ++(Triad number)
+        {
+            Triad count10 = new Triad(number.One+=10, number.Two+=10, number.Three+=10);
+            return count10;
+        }
+    }
+}
+// return One > triad.One || One == triad.One && Two > triad.Two && Three > triad.Two;
